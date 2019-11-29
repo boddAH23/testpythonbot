e91d736e0f8dcbbe6d8f9343ca6bc68868df865b
alex <a.befort@googlemail.com>
2019-11-29 12:21:56 +0100

    init

---
+++ Main.py
@@ -0,0 +1,30 @@
+import datetime
+
+from testbot import BotHandler
+
+greet_bot = BotHandler(token)
+greetings = ('hello', 'hi', 'greetings', 'sup')
+now = datetime.datetime.now()
+
+
+def main():
+    new_offset = None
+
+    while True:
+        greet_bot.get_updates(new_offset)
+
+        last_update = greet_bot.get_last_update()
+
+        last_update_id = last_update['update_id']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+
+        greet_bot.send_message(last_chat_id, 'Hallo  {}'.format(last_chat_name))
+
+        new_offset = last_update_id + 1
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
