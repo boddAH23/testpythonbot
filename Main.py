f7b0681c4d687d901e0d039dc0d826f91b4cbc5b
alex <a.befort@googlemail.com>
2019-12-06 10:10:11 +0100

    test

--- Main.py
+++ Main.py
@@ -10,18 +10,18 @@ now = datetime.datetime.now()
 def main():
     new_offset = None
 
-    while True:
-        greet_bot.get_updates(new_offset)
-
-        last_update = greet_bot.get_last_update()
-
-        last_update_id = last_update['update_id']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-
-        greet_bot.send_message(last_chat_id, 'Hallo  {}'.format(last_chat_name))
-
-        new_offset = last_update_id + 1
+    # while True:
+    #     greet_bot.get_updates(new_offset)
+    #
+    #     last_update = greet_bot.get_last_update()
+    #
+    #     last_update_id = last_update['update_id']
+    #     last_chat_id = last_update['message']['chat']['id']
+    #     last_chat_name = last_update['message']['chat']['first_name']
+    #
+    #     greet_bot.send_message(last_chat_id, 'Hallo  {}'.format(last_chat_name))
+    #
+    #     new_offset = last_update_id + 1
 
 if __name__ == '__main__':
     try:
