c30855907fa78765a74ead8b6adf4485215e37af
alex <a.befort@googlemail.com>
2019-12-06 10:44:42 +0100

    test

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
 import datetime
+import os
 
 from testbot import BotHandler
 
-greet_bot = BotHandler("765697015:AAFOyLksFpcUI6XLbZ-uXig6wdeSFmT7xxQ")
+token = os.environ.get('TOKEN', None)
+
+greet_bot = BotHandler("1036743508:AAGWl55n0XDuwhvVcgO2HbE0PINpB9qiUBA")
 greetings = ('hello', 'hi', 'greetings', 'sup')
 now = datetime.datetime.now()
 
@@ -10,18 +13,18 @@ now = datetime.datetime.now()
 def main():
     new_offset = None
 
-    # while True:
-    #     greet_bot.get_updates(new_offset)
-    #
-    #     last_update = greet_bot.get_last_update()
-    #
-    #     last_update_id = last_update['update_id']
-    #     last_chat_id = last_update['message']['chat']['id']
-    #     last_chat_name = last_update['message']['chat']['first_name']
-    #
-    #     greet_bot.send_message(last_chat_id, 'Hallo  {}'.format(last_chat_name))
-    #
-    #     new_offset = last_update_id + 1
+    while True:
+        greet_bot.get_updates(new_offset)
+
+        last_update = greet_bot.get_last_update()
+
+        last_update_id = last_update['update_id']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+
+        greet_bot.send_message(last_chat_id, 'Hallo  {}'.format(last_chat_name))
+
+        new_offset = last_update_id + 1
 
 if __name__ == '__main__':
     try:
