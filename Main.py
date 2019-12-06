6a1f86cc4c594f74ac27dc8608ea3e5cda2df69b
alex <a.befort@googlemail.com>
2019-12-06 11:10:34 +0100

    test

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import logging
 
 from testbot import BotHandler
 
@@ -13,17 +14,20 @@ def main():
     new_offset = None
 
     while True:
-        greet_bot.get_updates(new_offset)
+        try:
+            greet_bot.get_updates(new_offset)
 
-        last_update = greet_bot.get_last_update()
+            last_update = greet_bot.get_last_update()
 
-        last_update_id = last_update['update_id']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
+            last_update_id = last_update['update_id']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
 
-        greet_bot.send_message(last_chat_id, 'Hallo  {}'.format(last_chat_name))
+            greet_bot.send_message(last_chat_id, 'Hallo  {}'.format(last_chat_name))
 
-        new_offset = last_update_id + 1
+            new_offset = last_update_id + 1
+        except Exception as e:
+            logging.error(e)
 
 if __name__ == '__main__':
     try:
