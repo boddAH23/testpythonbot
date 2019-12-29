ff3a677540e274e58fe3d23566d1136d27f80199
alex <a.befort@googlemail.com>
2019-12-29 11:20:05 +0100

    refactor to webhook

--- Main.py
+++ Main.py
@@ -1,36 +1,50 @@
-import datetime
-import os
+
 import logging
+import os
+import random
+import sys
 
-from testbot import BotHandler
+from telegram.ext import Updater, CommandHandler
+
+# Enabling logging
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+
+# Getting mode, so we could define run function for local and Heroku setup
+mode = os.getenv("MODE")
+TOKEN = os.getenv("TOKEN")
+if mode == "dev":
+    def run(updater):
+        updater.start_polling()
+else:
+    def run(updater):
+        PORT = int(os.environ.get("PORT", "8443"))
+        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
+        updater.start_webhook(listen="0.0.0.0",
+                              port=PORT,
+                              url_path=TOKEN)
+        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
+
+def start_handler(bot, update):
+    # Creating a handler-function for /start command
+    logger.info("User {} started bot".format(update.effective_user["id"]))
+    update.message.reply_text("Hello from Python!\nPress /random to get random number")
 
 
-greet_bot = BotHandler(os.environ['TOKEN'])
-greetings = ('hello', 'hi', 'greetings', 'sup')
-now = datetime.datetime.now()
+def random_handler(bot, update):
+    # Creating a handler-function for /random command
+    number = random.randint(0, 10)
+    logger.info("User {} randomed number {}".format(update.effective_user["id"], number))
+    update.message.reply_text("Random number: {}".format(number))
 
-
-def main():
-    new_offset = None
-
-    while True:
-        try:
-            greet_bot.get_updates(new_offset)
-
-            last_update = greet_bot.get_last_update()
-
-            last_update_id = last_update['update_id']
-            last_chat_id = last_update['message']['chat']['id']
-            last_chat_name = last_update['message']['chat']['first_name']
-
-            greet_bot.send_message(last_chat_id, 'Hallo  {}'.format(last_chat_name))
-
-            new_offset = last_update_id + 1
-        except Exception as e:
-            logging.error(e)
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
+    logger.info("Starting bot")
+    updater = Updater(TOKEN, use_context=True)
+
+    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
+    updater.dispatcher.add_handler(CommandHandler("random", random_handler))
+
+    run(updater)
