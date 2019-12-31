29745e144b65d06fbc8aace3ff3107f9e24f68b2
alex <a.befort@googlemail.com>
2019-12-31 11:51:22 +0100

    Remove nullpointer

--- Main.py
+++ Main.py
@@ -37,8 +37,7 @@ def start_handler(bot, update):
 def random_handler(bot, update):
     # Creating a handler-function for /random command
     number = random.randint(0, 10)
-    logger.info("Update object:{}".format(update.user_data))
-    logger.info("User {} randomed number {}".format(update.effective_user["id"], number))
+    logger.info("Update object:{}".format(update))
     update.message.reply_text("Random number: {}".format(number))
 
 
